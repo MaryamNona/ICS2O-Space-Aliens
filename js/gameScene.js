0252e57c4a99e5a2fc87682cbbb62080a2193a10
MaryamNona <>
2021-06-07 00:25:52 +0000

    fixed tabs error

--- js/gameScene.js
+++ js/gameScene.js
@@ -12,7 +12,7 @@ class GameScene extends Phaser.Scene {
 
     this.background = null
     this.ship = null
-		this.fireMissile = false
+    this.fireMissile = false
   }
 
   init (data) {
@@ -25,25 +25,25 @@ class GameScene extends Phaser.Scene {
     // images
     this.load.image('starBackground', './assets/starBackground.png')
     this.load.image('ship', './assets/spaceShip.png')
-		this.load.image('missile', './assets/missile.png')
+    this.load.image('missile', './assets/missile.png')
   }
 
   create (data) {
     this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
-
+    
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
-
-		// create a group for the missiles
-		this.missileGroup = this.physics.add.group()
+    
+    // create a group for the missiles
+    this.missileGroup = this.physics.add.group()
   }
-
+  
   update (time, delta) {
     // called 60 times a second, hopefully
 
     const keyLeftObj = this.input.keyboard.addKey('LEFT')
     const keyRightObj = this.input.keyboard.addKey('RIGHT')
-		const keySpaceObj = this.input.keyboard.addKey('SPACE')
+    const keySpaceObj = this.input.keyboard.addKey('SPACE')
 
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
@@ -58,20 +58,20 @@ class GameScene extends Phaser.Scene {
         this.ship.x = 1920
       }
     }
-
-		if (keySpaceObj.isDown === true) {
-			if (this.fireMissile === false) {
-				// fire missile
-				this.fireMissile = true
-				const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
-				this.missileGroup.add(aNewMissile)
-			}
-		}
-
-		if (keySpaceObj.isUp === true) {
-			this.fireMissile = false
-		}
-  }
+    
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile === false) {
+        // fire missile
+        this.fireMissile = true
+        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
+        this.missileGroup.add(aNewMissile)
+      }
+    }
+    
+    if (keySpaceObj.isUp === true) {
+      this.fireMissile = false 
+    }
+  } 
 }
 
 export default GameScene
